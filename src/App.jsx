7d7984e7a9ebeb4d1ac62a9767d9c656be818a7b
AbhayParasharhere <83302622+AbhayParasharhere@users.noreply.github.com>
2024-03-31 21:18:43 -0700

    Add basic athentication with sign in with google, apple sign in is missing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,58 @@
 import "./App.scss";
+import React, { useMemo, useState } from "react";
 import Header from "./CommonComponents/Header";
+import {
+  signInEmailAndPassword,
+  signInwithGoogle,
+  signUpWithEmailAndPassword,
+  storeUserAdditionalDetails,
+} from "./Firebase/authentication";
+
+// Sign Up steps
+// First take in the email and password, confirmPassword in 1 page
+// Then create the user and provide next stage form of
+// name, phone number, address, dob, and photo
+// Then the additional details will be stored in another collection known as userDetail
+// with the uid of the user as a ref
 
 function App() {
+  const [response, setResponse] = useState(null);
+
+  const testSignUp = async () => {
+    const res = await signUpWithEmailAndPassword(
+      "abhay7171_9@gmail.com",
+      "123456"
+    );
+    // print the response
+    console.log(res);
+    setResponse(res);
+  };
+
+  const testGoogleSignIn = async () => {
+    const res = await signInwithGoogle();
+    console.log(res);
+    setResponse(res);
+  };
+
+  const storeAdditionalDetails = async () => {
+    // store the additional details in the userDetail collection
+
+    const res = await storeUserAdditionalDetails(response?.user?.uid, {
+      phone_number: "1234567890",
+      address: "123, abc street",
+      dob: "01-01-2000",
+      name: response?.user?.displayName || "Abhay",
+    });
+    console.log("In app store additional details fx", res);
+  };
+
   return (
     <>
+      <button onClick={testSignUp}>Test SignUp</button>
+      <button onClick={storeAdditionalDetails} disabled={!response}>
+        Test Adding Other details
+      </button>
+      <button onClick={testGoogleSignIn}>Test Google Sign in</button>
       <Header />
     </>
   );
--- src/Firebase/firebaseConfig.jsx
+++ src/Firebase/firebaseConfig.jsx
@@ -6,6 +6,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,3 +27,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const analytics = getAnalytics(app);
+export const db = getFirestore(app);
