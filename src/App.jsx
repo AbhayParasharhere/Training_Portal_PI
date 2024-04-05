3d8879a6e703a14d25153c88b3b54014fd6ce2b0
AbhayParasharhere <83302622+AbhayParasharhere@users.noreply.github.com>
2024-04-04 21:13:42 -0700

    Fix main

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.scss";
-import React, { useState } from "react";
 import Header from "./CommonComponents/Header";
 import SignUp from "./Pages/Signup";
-import AllCourses from "./Pages/AllCourse
+import AllCourses from "./Pages/AllCourses";
 import CalendarModal from "./CommonComponents/CalendarModal";
 import Button from "./CommonComponents/Button";
 import RegisterComponent from "./Components/Register/RegisterComponent";
 import Register_2Component from "./Components/Register_2/Register_2Component";
 import LoginComponent from "./Components/Login/LoginComponent";
-
 
 // Sign Up steps
 // First take in the email and password, confirmPassword in 1 page
