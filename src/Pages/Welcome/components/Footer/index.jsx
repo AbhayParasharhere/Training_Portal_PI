17dea4af8346288e3cca8c69adcc94c7d77452c1
abhi-rzoro <144512510+abhi-rzoro@users.noreply.github.com>
2024-04-16 06:04:47 +0530

    responsive complete for welcome footer component

--- src/Pages/Welcome/components/Footer/index.jsx
+++ src/Pages/Welcome/components/Footer/index.jsx
@@ -38,6 +38,40 @@ export default function Footer() {
           </div>
         </div>
       </div>
+      <div className={styles["footer--mobile-container"]}>
+        <div className={styles["footer--mobile-left-links-container"]}>
+          {" "}
+          <p className={styles["footer--links"]}>Contact</p>
+          <p className={styles["footer--links"]}>Help</p>
+          <p className={styles["footer--links"]}>FAQS</p>
+        </div>
+        <div className={styles["footer--mobile-center-links-container"]}>
+          {" "}
+          <p className={styles["footer--links"]}>PRIVACY & POLICY</p>
+          <p className={styles["footer--links"]}>TERMS & CONDITIONS</p>
+        </div>
+        <div className={styles["footer--mobile-social-media-container"]}>
+          {" "}
+          <img
+            className={styles["footer--social-media-icons"]}
+            src={twitterLogo}
+          />
+          <img
+            className={styles["footer--social-media-icons"]}
+            src={instagramLogo}
+          />{" "}
+          <img
+            className={styles["footer--social-media-icons"]}
+            src={linkedInLogo}
+          />
+        </div>
+      </div>
+      <p className={styles["footer--copyright-text-mobile"]}>
+        Copyright © 2024
+      </p>
+      <p className={styles["footer--copyright-text-mobile"]}>
+        Punjab Insurance All Rights Reserved
+      </p>
       <p className={styles["footer--copyright-text"]}>
         Copyright © 2024 Punjab Insurance All Rights Reserved
       </p>
