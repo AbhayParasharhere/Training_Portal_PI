64dc1505c7becf6a1cf36cb5d9b046b60d2e7788
AbhayParasharhere <83302622+AbhayParasharhere@users.noreply.github.com>
2024-04-17 15:17:34 -0700

    Add prototypes for the kpi measuring functions

---
+++ src/Firebase/kpi.jsx
@@ -0,0 +1,17 @@
+import { doc } from "./firebaseConfig";
+
+// Function to get all sales data for a particular user
+
+// Function to get all clients data for a particular user
+
+// Functions to get all sales data for a particular user within a particular date range
+
+// Function to get all clients data for a particular user within a particular date range
+
+// Function to get the total logged in time for a particular user
+// In order to do this, we need to store a document in the userDetails document for that user in a subcollection called as loginCount whenever a user logs in
+
+// Function to get the total logged in time for a particular user within a particular date range
+
+// Function to get the progress for a specified course for a particular user
+// In order to do this, we need to store a document in videoProgress collection whenever a user watches a video
