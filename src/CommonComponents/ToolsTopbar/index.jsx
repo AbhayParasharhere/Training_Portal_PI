7e7e6cd60f852769ce60b04802e122f9a478f156
Naman <116908089+nbatra752@users.noreply.github.com>
2024-04-18 09:49:18 +0530

    Padding fixed

--- src/CommonComponents/ToolsTopbar/index.jsx
+++ src/CommonComponents/ToolsTopbar/index.jsx
@@ -3,20 +3,18 @@ import styles from "./styles.module.scss";
 
 export default function ToolsTopbar() {
   return (
-    <div className={styles["ClientInfo-wrapper-topbar"]}>
-      <div className={styles["ClientInfo-wrapper-topbar-div"]}>
-        <div className={styles["ClientInfo-wrapper-topbar-text"]}>
-          Sales Resources
-        </div>
-        <div className={styles["ClientInfo-wrapper-topbar-text"]}>
-          Marketing Resources
-        </div>
-        <div className={styles["ClientInfo-wrapper-topbar-text"]}>
-          Product Links
-        </div>
-        <div className={styles["ClientInfo-wrapper-topbar-text"]}>
-          Finance Calculator
-        </div>
+    <div className={styles["ClientInfo-wrapper-topbar-div"]}>
+      <div className={styles["ClientInfo-wrapper-topbar-text"]}>
+        Sales Resources
+      </div>
+      <div className={styles["ClientInfo-wrapper-topbar-text"]}>
+        Marketing Resources
+      </div>
+      <div className={styles["ClientInfo-wrapper-topbar-text"]}>
+        Product Links
+      </div>
+      <div className={styles["ClientInfo-wrapper-topbar-text"]}>
+        Finance Calculator
       </div>
     </div>
   );
