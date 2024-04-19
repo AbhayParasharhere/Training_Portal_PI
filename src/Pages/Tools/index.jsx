2a004c11de65f7feb6244a53bc5d14920adfbe28
Naman <116908089+nbatra752@users.noreply.github.com>
2024-04-19 09:26:27 +0530

    list add

--- src/Pages/Tools/index.jsx
+++ src/Pages/Tools/index.jsx
@@ -6,12 +6,18 @@ import Group from "./Images/Group.png";
 import arrow_right from "./Images/arrow_right.png";
 
 export default function Tools() {
+  const list = [
+    { filename: "File name" },
+    { filename: "File name" },
+    { filename: "File name" },
+    { filename: "File name" },
+  ];
   return (
     <div className={styles["Tools-main"]}>
-      <div>
-        <Sidebar />
-        <div className={styles["ClientInfo-wrapper"]}>
-          <ToolsTopbar />
+      <Sidebar />
+      <div className={styles["ClientInfo-wrapper"]}>
+        <ToolsTopbar />
+        {list.map((item) => (
           <div className={styles["ClientInfo-wrapper-main"]}>
             <div className={styles["ClientInfo-wrapper-main-content"]}>
               <div className={styles["ClientInfo-wrapper-main-content-div"]}>
@@ -21,13 +27,13 @@ export default function Tools() {
                   <img src={Group} height="28px" />
                 </div>
                 <div className={styles["ClientInfo-wrapper-main-content-file"]}>
-                  File name
+                  {item.filename}
                 </div>
               </div>
               <img src={arrow_right} height="28px" />
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
