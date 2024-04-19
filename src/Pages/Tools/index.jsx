31baf1a33ce5a3c9295fc5800d3340b137a11e75
Naman <116908089+nbatra752@users.noreply.github.com>
2024-04-19 09:11:28 +0530

    Tools Page

--- src/Pages/Tools/index.jsx
+++ src/Pages/Tools/index.jsx
@@ -7,25 +7,28 @@ import arrow_right from "./Images/arrow_right.png";
 
 export default function Tools() {
   return (
-    // <div className={styles["Tools-main"]}>
-    <div>
-      <Sidebar />
-      {/* <div className={styles["ClientInfo-wrapper"]}>
-        <ToolsTopbar />
-        <div className={styles["ClientInfo-wrapper-main"]}>
-          <div className={styles["ClientInfo-wrapper-main-content"]}>
-            <div className={styles["ClientInfo-wrapper-main-content-div"]}>
-              <div className={styles["ClientInfo-wrapper-main-content-image"]}>
-                <img src={Group} height="28px" />
+    <div className={styles["Tools-main"]}>
+      <div>
+        <Sidebar />
+        <div className={styles["ClientInfo-wrapper"]}>
+          <ToolsTopbar />
+          <div className={styles["ClientInfo-wrapper-main"]}>
+            <div className={styles["ClientInfo-wrapper-main-content"]}>
+              <div className={styles["ClientInfo-wrapper-main-content-div"]}>
+                <div
+                  className={styles["ClientInfo-wrapper-main-content-image"]}
+                >
+                  <img src={Group} height="28px" />
+                </div>
+                <div className={styles["ClientInfo-wrapper-main-content-file"]}>
+                  File name
+                </div>
               </div>
-              <div className={styles["ClientInfo-wrapper-main-content-file"]}>
-                File name
-              </div>
+              <img src={arrow_right} height="28px" />
             </div>
-            <img src={arrow_right} height="28px" />
           </div>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
