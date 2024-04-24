2a9b434d985da087829ee286e6ea5dedaf8cc9c5
abhi-rzoro <144512510+abhi-rzoro@users.noreply.github.com>
2024-04-24 17:18:19 +0530

    statistics-page completed with responsive

--- src/Pages/Statistics/index.jsx
+++ src/Pages/Statistics/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import profileImage from "./images/sample-image.png";
+import StatisticsChart from "./component/chart";
 
 export default function Statistics() {
   const generalStatData = [
@@ -8,6 +9,7 @@ export default function Statistics() {
     { stat: 22, title1: "Videos", title2: "Watched", bar: true },
     { stat: 5, title1: "Login in", title2: "this week", bar: false },
   ];
+
   const renderGeneralStat = generalStatData.map((stat) => {
     return (
       <div className={styles["statistics--general-progress-container"]}>
@@ -31,6 +33,52 @@ export default function Statistics() {
       </div>
     );
   });
+  const graphData = [
+    {
+      title: "Client Status",
+      labels: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      data: [10, 20, 15, 30, 18, 29, 40, 25, 30, 10, 11, 14],
+    },
+    {
+      title: "Sales Status",
+      labels: ["M", "T", "W", "T", "F", "S", "S"],
+      data: [20, 15, 30, 25, 30, 11, 14],
+    },
+  ];
+
+  const renderGraph = graphData.map((graph) => {
+    return (
+      <div className={styles["statistics--graph-container"]}>
+        <div className={styles["statistics--client-graph-stat-switch"]}>
+          <p className={styles["statistics--graph-title"]}>{graph.title}</p>
+          <div className={styles["statistics--time-switch-button-container"]}>
+            <button className={styles["statistics--time-switch-button"]}>
+              Weekly
+            </button>
+            <button className={styles["statistics--time-switch-button"]}>
+              Total
+            </button>
+          </div>
+        </div>
+
+        <StatisticsChart labels={graph.labels} data={graph.data} />
+      </div>
+    );
+  });
+
   return (
     <div className={styles["statistics--main-container"]}>
       <div className={styles["statistics--general-container"]}>
@@ -45,7 +93,22 @@ export default function Statistics() {
           <p className={styles["statistics--user-name"]}>Gurpreet singh</p>
         </div>
       </div>
-      <div className={styles["statistics--graph-container"]}></div>
+      {/* <div className={styles["statistics--graph-container"]}>
+        <div className={styles["statistics--client-graph-stat-switch"]}>
+          <p className={styles["statistics--graph-title"]}>Client Status</p>
+          <div className={styles["statistics--time-switch-button-container"]}>
+            <button className={styles["statistics--time-switch-button"]}>
+              Weekly
+            </button>
+            <button className={styles["statistics--time-switch-button"]}>
+              Total
+            </button>
+          </div>
+        </div>
+
+        <StatisticsChart />
+      </div> */}
+      {renderGraph}
     </div>
   );
 }
