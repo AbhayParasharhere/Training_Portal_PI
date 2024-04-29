cca7d7c6c47d4f0be86c3af1aeb6a403b3452844
AbhayParasharhere <83302622+AbhayParasharhere@users.noreply.github.com>
2024-04-28 21:01:46 -0700

    Change login count

---
+++ src/context/primaryDataContext.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// We will use this context to fetch the primary data
+// All announcements
+// All the user details
+// All the enrolled courses
+// All the appoitments
+// All the clients
+// All the sales
+
+/// Change to be made
+// Video array in the course unordered
+// Login count should be an array of dates in the user details
+// Videos watched array in user details containing the video id unordered
+const primaryDataContext = () => {
+  return <div>primaryDataContext</div>;
+};
+
+export default primaryDataContext;
