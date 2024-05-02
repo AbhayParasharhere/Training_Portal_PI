0dc5e4137ec796298b77fdcaae4c97f8b2f10019
abhi-rzoro <144512510+abhi-rzoro@users.noreply.github.com>
2024-05-02 07:21:30 +0530

    get clients birthdays and anniversary link to tablet dashboard

--- src/Pages/Dashboard/components/SatisticsSummary/index.jsx
+++ src/Pages/Dashboard/components/SatisticsSummary/index.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import styles from "./styles.module.scss";
 import samplePhoto from "./images/profile-photo.png";
+import calendarIcon from "./images/calendar.png";
 import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJs,
@@ -12,12 +13,15 @@ import cakeIcon from "./images/cakeIcon.png";
 import clientPhoto from "./images/client-sample-image.png";
 import { useNavigate } from "react-router-dom";
 import secureLocalStorage from "react-secure-storage";
+import { PrimaryDataContext } from "../../../../context/primaryDataContext";
 
 ChartJs.register(CategoryScale, LinearScale, BarElement);
 
 export default function StatsSummary() {
   const userDetails = secureLocalStorage.getItem("userDetails");
   const navigate = useNavigate();
+  const primaryDataContext = useContext(PrimaryDataContext);
+  const clients = primaryDataContext?.clients;
   const [graphData, setGraphData] = useState([
     { name: "M", value: "12" },
     { name: "T", value: "5" },
@@ -27,6 +31,98 @@ export default function StatsSummary() {
     { name: "S", value: "9" },
     { name: "S", value: "7" },
   ]);
+  function getUpcomingEvents(clientData) {
+    const currentDate = new Date();
+    const currentDay = currentDate.getDate();
+    const currentMonth = currentDate.getMonth() + 1; // Month starts from 0
+
+    // Combine all events into a single array
+    const allEvents = [];
+
+    clientData?.forEach((client) => {
+      const dobParts = client.DOB.split("-");
+      const anniversaryParts = client.anniversary.split("-");
+      const dobMonth = parseInt(dobParts[1], 10);
+      const dobDay = parseInt(dobParts[2], 10);
+      const anniversaryMonth = parseInt(anniversaryParts[1], 10);
+      const anniversaryDay = parseInt(anniversaryParts[2], 10);
+
+      // Check if DOB is today or within a week (7 days)
+      if (
+        (dobMonth === currentMonth &&
+          dobDay >= currentDay &&
+          dobDay - currentDay <= 7) ||
+        (dobMonth === currentMonth &&
+          dobDay < currentDay &&
+          currentDay - dobDay <= 7)
+      ) {
+        allEvents.push({ ...client, eventType: "Birthday" });
+      }
+
+      // Check if anniversary is today or within a week (7 days)
+      if (
+        (anniversaryMonth === currentMonth &&
+          anniversaryDay >= currentDay &&
+          anniversaryDay - currentDay <= 7) ||
+        (anniversaryMonth === currentMonth &&
+          anniversaryDay < currentDay &&
+          currentDay - anniversaryDay <= 7)
+      ) {
+        allEvents.push({ ...client, eventType: "Anniversary" });
+      }
+    });
+
+    // Sort events by the latest event first
+    allEvents.sort((a, b) => {
+      const dateA = new Date(
+        2000,
+        a.eventType === "Birthday"
+          ? a.DOB.split("-")[2]
+          : a.anniversary.split("-")[2]
+      );
+      const dateB = new Date(
+        2000,
+        b.eventType === "Birthday"
+          ? b.DOB.split("-")[2]
+          : b.anniversary.split("-")[2]
+      );
+
+      return dateA - dateB;
+    });
+
+    return allEvents;
+  }
+
+  let upcomingEvents = [];
+  if (clients) {
+    upcomingEvents = getUpcomingEvents(clients);
+  }
+  const renderClientEvent = upcomingEvents?.map((client) => {
+    return (
+      <div
+        className={styles["home--client-birthday"]}
+        onClick={() => navigate(`/client-detail/${client.id}`)}
+        style={{ cursor: "pointer" }}
+      >
+        <div className={styles["home--client-birthday-inner-container"]}>
+          <img src={clientPhoto} className={styles["home--client-image"]} />
+          <div className={styles["home--birthday-name-date-container"]}>
+            <p className={styles["home--client-name"]}>{client.name}</p>
+            <p className={styles["home--birthday-date"]}>
+              {client.eventType === "Anniversary"
+                ? client.anniversary
+                : client.DOB}{" "}
+            </p>{" "}
+          </div>
+        </div>
+        <img
+          src={client.eventType === "Birthday" ? cakeIcon : calendarIcon}
+          className={styles["home--cake-icon"]}
+        />
+      </div>
+    );
+  });
+
   return (
     <div className={styles["statsSummary--main-container"]}>
       <div className={styles["statsSummary--profile-container"]}>
@@ -157,46 +253,7 @@ export default function StatsSummary() {
           Upcoming Clients Bithdays And Anniversary
         </p>
         <div className={styles["home--client-birthday-list"]}>
-          <div className={styles["home--client-birthday"]}>
-            <div className={styles["home--client-birthday-inner-container"]}>
-              <img src={clientPhoto} className={styles["home--client-image"]} />
-              <div className={styles["home--birthday-name-date-container"]}>
-                <p className={styles["home--client-name"]}>Client Name</p>
-                <p className={styles["home--birthday-date"]}>
-                  April 24, 2024
-                </p>{" "}
-              </div>
-            </div>
-            <img src={cakeIcon} className={styles["home--cake-icon"]} />
-          </div>
-        </div>
-        <div className={styles["home--client-birthday-list"]}>
-          <div className={styles["home--client-birthday"]}>
-            <div className={styles["home--client-birthday-inner-container"]}>
-              <img src={clientPhoto} className={styles["home--client-image"]} />
-              <div className={styles["home--birthday-name-date-container"]}>
-                <p className={styles["home--client-name"]}>Client Name</p>
-                <p className={styles["home--birthday-date"]}>
-                  April 24, 2024
-                </p>{" "}
-              </div>
-            </div>
-            <img src={cakeIcon} className={styles["home--cake-icon"]} />
-          </div>
-        </div>{" "}
-        <div className={styles["home--client-birthday-list"]}>
-          <div className={styles["home--client-birthday"]}>
-            <div className={styles["home--client-birthday-inner-container"]}>
-              <img src={clientPhoto} className={styles["home--client-image"]} />
-              <div className={styles["home--birthday-name-date-container"]}>
-                <p className={styles["home--client-name"]}>Client Name</p>
-                <p className={styles["home--birthday-date"]}>
-                  April 24, 2024
-                </p>{" "}
-              </div>
-            </div>
-            <img src={cakeIcon} className={styles["home--cake-icon"]} />
-          </div>
+          {renderClientEvent}
         </div>
       </div>
       {/*Birthday container for tablet responsive ends*/}
