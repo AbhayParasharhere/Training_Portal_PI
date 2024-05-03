4eba2c3ab1b39967fd8e4d94e915428ef0f38edd
AbhayParasharhere <83302622+AbhayParasharhere@users.noreply.github.com>
2024-05-03 09:09:29 -0700

    Add state for data collection in posts, and chnage multiselect column to single choice

--- src/Pages/PostQues/components/AddPost/index.jsx
+++ src/Pages/PostQues/components/AddPost/index.jsx
@@ -3,33 +3,74 @@ import styles from "./styles.module.scss";
 import { toast } from "react-toastify";
 
 export default function AddPost(props) {
+  const [doubt, setDoubt] = React.useState("");
+  const [postCategory, setPostCategory] = React.useState("compliance");
+
+  const handleCategoryChange = (event) => {
+    setPostCategory(event.target.value);
+  };
+  // console.log(doubt, postCategory);
   return (
     <div className={styles["addPost--main-container"]}>
       <p className={styles["addPost--doubt-title"]}>Doubt</p>
-      <textarea className={styles["addPost--text-area"]} />
+      <textarea
+        className={styles["addPost--text-area"]}
+        onChange={(e) => setDoubt(e.target.value)}
+      />
       <div style={{ marginTop: 10 }}>
-        <p className={styles["addPost--choose-title"]}>Choose post catagory:</p>
+        <p className={styles["addPost--choose-title"]}>Choose post category:</p>
         <div style={{ marginBottom: 20 }}>
-          <input type="radio" id="compliance" />
+          <input
+            type="radio"
+            id="compliance"
+            name="compliance"
+            value="compliance"
+            checked={postCategory === "compliance"}
+            onChange={handleCategoryChange}
+          />
           <label for="compliance" className={styles["addPost--label-text"]}>
             Compliance
           </label>
         </div>
         <div style={{ marginBottom: 20 }}>
-          <input type="radio" id="compliance" />
-          <label for="compliance" className={styles["addPost--label-text"]}>
+          <input
+            type="radio"
+            id="technical_issue"
+            name="technical_issue"
+            value="technical_issue"
+            checked={postCategory === "technical_issue"}
+            onChange={handleCategoryChange}
+          />
+          <label
+            for="technical_issue"
+            className={styles["addPost--label-text"]}
+          >
             Technical Issue
           </label>
         </div>
         <div style={{ marginBottom: 20 }}>
-          <input type="radio" id="compliance" />
-          <label for="compliance" className={styles["addPost--label-text"]}>
+          <input
+            type="radio"
+            id="general"
+            name="general"
+            value="general"
+            checked={postCategory === "general"}
+            onChange={handleCategoryChange}
+          />
+          <label for="general" className={styles["addPost--label-text"]}>
             General
           </label>
         </div>
         <div style={{ marginBottom: 20 }}>
-          <input type="radio" id="compliance" />
-          <label for="compliance" className={styles["addPost--label-text"]}>
+          <input
+            type="radio"
+            id="insurance"
+            name="insurance"
+            value="insurance"
+            checked={postCategory === "insurance"}
+            onChange={handleCategoryChange}
+          />
+          <label for="insurance" className={styles["addPost--label-text"]}>
             Insurance
           </label>
         </div>
