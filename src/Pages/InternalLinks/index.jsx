717955558fb7b29db32964247db7a282cd2f2ce5
AbhayParasharhere <83302622+AbhayParasharhere@users.noreply.github.com>
2024-05-05 21:04:17 -0700

    Complete tools page

--- src/Pages/InternalLinks/index.jsx
+++ src/Pages/InternalLinks/index.jsx
@@ -1,101 +1,113 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import fileIcon from "./images/file-icon.png";
+import LinkIcon from "./images/all-links-red-icon.png";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function InternalLinks() {
-  const linksData = [
-    {
-      title: "Document 1",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
-      link: "https://youtube.com",
-    },
-    {
-      title: "Document 2",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
-      link: "https://youtube.com",
-    },
-    {
-      title: "Document 3",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
-      link: "https://youtube.com",
-    },
-    {
-      title: "Document 4",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
-      link: "https://youtube.com",
-    },
-    ,
-    {
-      title: "Document 4",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
-      link: "https://youtube.com",
-    },
-    ,
-    {
-      title: "Document 4",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
-      link: "https://youtube.com",
-    },
-    ,
-    {
-      title: "Document 4",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
-      link: "https://youtube.com",
-    },
-    ,
-    {
-      title: "Document 4",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
-      link: "https://youtube.com",
-    },
-    ,
-    {
-      title: "Document 4",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
-      link: "https://youtube.com",
-    },
-    ,
-    {
-      title: "Document 4",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
-      link: "https://youtube.com",
-    },
-  ];
-  const renderLinks = linksData.map((link) => {
-    return (
-      <div className={styles["internalLinks--link-container"]}>
-        <p className={styles["internalLinks--link-title"]}>{link.title}</p>
-        <div className={styles["internalLinks--link-desc-container"]}>
-          <p className={styles["internalLinks--link-desc"]}>{link.desc}</p>
-          <a
-            style={{ textDecoration: "none" }}
-            className={styles["internalLinks--click"]}
-            href={link.link}
-            target="_blank"
-          >
-            Click to View
-          </a>
+  const navigate = useNavigate();
+  // const linksData = [
+  //   {
+  //     title: "Document 1",
+  //     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  //     link: "https://youtube.com",
+  //   },
+  //   {
+  //     title: "Document 2",
+  //     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  //     link: "https://youtube.com",
+  //   },
+  //   {
+  //     title: "Document 3",
+  //     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  //     link: "https://youtube.com",
+  //   },
+  //   {
+  //     title: "Document 4",
+  //     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  //     link: "https://youtube.com",
+  //   },
+  //   ,
+  //   {
+  //     title: "Document 4",
+  //     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  //     link: "https://youtube.com",
+  //   },
+  //   ,
+  //   {
+  //     title: "Document 4",
+  //     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  //     link: "https://youtube.com",
+  //   },
+  //   ,
+  //   {
+  //     title: "Document 4",
+  //     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  //     link: "https://youtube.com",
+  //   },
+  //   ,
+  //   {
+  //     title: "Document 4",
+  //     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  //     link: "https://youtube.com",
+  //   },
+  //   ,
+  //   {
+  //     title: "Document 4",
+  //     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  //     link: "https://youtube.com",
+  //   },
+  //   ,
+  //   {
+  //     title: "Document 4",
+  //     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  //     link: "https://youtube.com",
+  //   },
+  // ];
+  const linksData = useLocation()?.state?.link;
+  let renderLinks = [];
+  if (linksData) {
+    console.log("Links data", linksData);
+    renderLinks = linksData?.subsections?.map((link) => {
+      return (
+        <div className={styles["internalLinks--link-container"]}>
+          <p className={styles["internalLinks--link-title"]}>{link.title}</p>
+          <div className={styles["internalLinks--link-desc-container"]}>
+            <p className={styles["internalLinks--link-desc"]}>
+              {link.description}
+            </p>
+            <a
+              style={{ textDecoration: "none" }}
+              className={styles["internalLinks--click"]}
+              href={link.link}
+              target="_blank"
+            >
+              Click to View
+            </a>
+          </div>
         </div>
-      </div>
-    );
-  });
-
+      );
+    });
+  }
   return (
     <div className={styles["internalLinks--main-container"]}>
-      <p className={styles["internalLinks--go-back"]}>{"<"}Go back</p>
+      <p
+        className={styles["internalLinks--go-back"]}
+        onClick={() => navigate(-1)}
+      >
+        {"<"}Go back
+      </p>
       <div className={styles["internalLinks--inner-container"]}>
         <div className={styles["internalLinks--link-catagory"]}>
-          <img src={fileIcon} className={styles["internalLinks--file-icon"]} />
+          <img src={LinkIcon} className={styles["internalLinks--file-icon"]} />
           <p className={styles["internalLinks--link-catagory-text"]}>
-            Insurance Documents
+            {String(linksData?.name)[0].toUpperCase() +
+              linksData?.name.slice(1)}
           </p>
         </div>
         <div className={styles["internalLinks--link-list-container"]}>
           {renderLinks}
         </div>
-        {/* <div >
-            <div></div>
-        </div> */}
       </div>
     </div>
   );
