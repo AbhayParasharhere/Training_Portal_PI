147c1e91130e64fa0c05398bf17ec1ca4421e1da
Naman <116908089+nbatra752@users.noreply.github.com>
2024-05-08 09:07:47 +0530

    Admin panel rebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import "./App.scss";
+import { useEffect } from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
   Route,
 } from "react-router-dom";
-import Header from "./CommonComponents/Header";
-import SignUp from "./Pages/Signup";
-import AllCourses from "./Pages/AllCourses";
-import CalendarModal from "./CommonComponents/CalendarModal";
-import Button from "./CommonComponents/Button";
-import Announcement from "./CommonComponents/Announcement";
 import Login from "./Pages/Login";
 import AdminConsole from "./Pages/AdminConsole";
 import AdminPanel from "./Pages/AdminPanel";
@@ -25,12 +20,47 @@ import VideoTutorial from "./Pages/Welcome/components/VideoTutorial";
 import Welcome from "./Pages/Welcome";
 import Dashboard from "./Pages/Dashboard";
 
+import SidebarLayout from "./Layouts/Sidebar";
+import FAQSupport from "./Pages/FAQSupport";
+
+import RegisterComponent from "./Pages/SignupPage/Components/Register";
+import Register_2Component from "./Pages/SignupPage/Components/RegisterTwo";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import AnnouncementPage from "./Pages/Announcement";
+import Webinar from "./Pages/Webinar";
+import Compliance from "./Pages/Compliance";
+import Statistics from "./Pages/Statistics";
+import Course from "./Pages/Course";
+
+import ClientComponent from "./Pages/ClientPage/Components/ClientComponent";
 import Tools from "./Pages/Tools";
 
 import SalesAdding from "./Pages/SalesAdding";
-import SidebarLayout from "./Layouts/Sidebar";
-import RegisterComponent from "./Pages/SignupPage/Components/Register/RegisterComponent";
-import Register_2Component from "./Pages/SignupPage/Components/RegisterTwo/RegisterTwoComponent";
+import ProductLinks from "./Pages/ProductLinks";
+
+import CourseDetail from "./Pages/Course/components/CourseDetail";
+import NoPagesElement from "./CommonComponents/NoPageError";
+
+import Meet from "./CommonComponents/Meet";
+
+import AdminConsole from "./Pages/AdminConsole";
+import InternalLinks from "./Pages/InternalLinks";
+
+import ClientDetail from "./Pages/ClientDetail";
+import ClientData from "./Pages/ClientDetail";
+import ClientInfo from "./Pages/Form";
+import PostQues from "./Pages/PostQues";
+import PurchasedPolicy from "./Pages/PurchasedPolicy";
+import AllLinks from "./Pages/AllLinks";
+import Contact from "./Pages/Contact";
+import ContactDetails from "./Pages/Contact";
+import WebinarForm from "./Pages/WebinarForm";
+import CalendarModal from "./CommonComponents/CalendarModal";
+import UploadForm from "./CommonComponents/UploadDocument";
+import MobileBirthdays from "./Pages/Dashboard/components/MobileBirthdays";
+import MobileNotifications from "./Pages/Dashboard/components/MobileNotifications";
+import ProductCourses from "./Pages/CourseProducts";
 
 // Sign Up steps
 // First take in the email and password, confirmPassword in 1 page
@@ -50,15 +80,46 @@ const router = createBrowserRouter(
       <Route path="/admin" element={<AdminPanel />} />
       <Route element={<SidebarLayout />} path="/">
         <Route index element={<Dashboard />} />
-        <Route path="/Add-Sales" element={<SalesAdding />} />
+        <Route path="/addSales" element={<SalesAdding />} />
+        <Route path="/support" element={<FAQSupport />} />
+        <Route path="/announcement" element={<AnnouncementPage />} />
+        <Route path="/webinar" element={<Webinar />} />
+        <Route path="/compliance" element={<Compliance />} />
+        <Route path="/statistics" element={<Statistics />} />
+        <Route path="/birthdays" element={<MobileBirthdays />} />
+        <Route path="/notifications" element={<MobileNotifications />} />
+
+        <Route path="/courses" element={<Course />} />
+
+        <Route path="/productlinks" element={<ProductLinks />} />
+        <Route path="/add-webinar" element={<WebinarForm />} />
+        <Route path="/clients" element={<ClientComponent />} />
+        <Route path="/tools" element={<Tools />} />
+        <Route path="/courses/:courseId" element={<CourseDetail />} />
+        <Route path="/contact" element={<ContactDetails />} />
+        <Route path="/productCourses" element={<ProductCourses />} />
+
+        <Route path="/admin-course-add" element={<AdminConsole />} />
+        <Route path="/all-links" element={<AllLinks />} />
+        <Route path="/internal-links" element={<InternalLinks />} />
+        <Route path="/client-detail/:clientId" element={<ClientData />}>
+          <Route index element={<ClientInfo />} />
+          <Route path="policies" element={<PurchasedPolicy />} />
+          <Route path="goals" element={<ClientInfo />} />
+        </Route>
+        <Route path="/connect" element={<PostQues />} />
+        <Route path="/add-document" element={<UploadForm />} />
       </Route>
     </>
   )
 );
 function App() {
   return (
-    <div style={{ display: "flex", gap: "40px", flexDirection: "column" }}>
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
       <RouterProvider router={router} />
+      <ToastContainer />
     </div>
   );
 }
