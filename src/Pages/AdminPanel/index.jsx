a09a1f36fa848489e244ae2d1d7b3ee4f7b47938
Naman <116908089+nbatra752@users.noreply.github.com>
2024-05-08 08:57:23 +0530

    Add Admin Panel Changes

--- src/Pages/AdminPanel/index.jsx
+++ src/Pages/AdminPanel/index.jsx
@@ -3,104 +3,211 @@ import styles from "./styles.module.scss";
 import { Button } from "@mui/material";
 import add from "./Images/cloud_add.png";
 import upload from "./Images/upload_line.png";
+import plus from "./Images/close_line.png";
+import Sidebar from "../../CommonComponents/Sidebar";
 
 const AdminPanel = () => {
   return (
-    <div className={styles["AdminPanel-wrapper"]}>
-      <div className={styles["AdminPanel-wrapper-cover"]}>
-        <div className={styles["AdminPanel-wrapper-course-cover"]}>
-          <label>Course Cover</label>
-          <div className={styles["AdminPanel-wrapper-course-cover-div"]}>
-            <img src={add} />
-            <div className={styles["AdminPanel-wrapper-course-cover-div-head"]}>
-              Choose a file or drag & drop it here
-            </div>
-            <div className={styles["AdminPanel-wrapper-course-cover-div-text"]}>
-              JPEG, PNG and PDG formats, up to 50MB
-            </div>
-            <div className={styles["browseButton"]}>
-              <Button
-                className={styles["browseAction"]}
-                disableElevation={true}
-                variant="contained"
-                sx={{
-                  textTransform: "none",
-                  color: "#54575c",
-                  fontSize: "15",
-                  background: "#fff",
-                  border: "#cbd0dc solid 1px",
-                  borderRadius: "8px",
-                  "&:hover": { background: "#fff" },
-                  height: 34,
-                }}
+    <div className={styles["AdminPanel-main"]}>
+      <Sidebar />
+      <div className={styles["AdminPanel-wrapper"]}>
+        <label className={styles["AdminPanel-wrapper-course-title-label"]}>
+          Course Cover
+        </label>
+        <div className={styles["AdminPanel-wrapper-cover"]}>
+          <div className={styles["AdminPanel-wrapper-cover-div"]}>
+            <div className={styles["AdminPanel-wrapper-course-cover"]}>
+              <div className={styles["AdminPanel-wrapper-course-cover-div"]}>
+                <img src={add} />
+                <div
+                  className={styles["AdminPanel-wrapper-course-cover-div-head"]}
+                >
+                  Choose a file or drag & drop it here
+                </div>
+                <div
+                  className={styles["AdminPanel-wrapper-course-cover-div-text"]}
+                >
+                  JPEG, PNG and PDG formats, up to 50MB
+                </div>
+                <div className={styles["browseButton"]}>
+                  <Button
+                    className={styles["browseAction"]}
+                    disableElevation={true}
+                    variant="contained"
+                    sx={{
+                      textTransform: "none",
+                      color: "#54575c",
+                      fontSize: "15",
+                      background: "#fff",
+                      border: "#cbd0dc solid 1px",
+                      borderRadius: "8px",
+                      "&:hover": { background: "#fff" },
+                      height: 34,
+                    }}
+                  >
+                    Browse File
+                  </Button>
+                </div>
+              </div>
+
+              <div className={styles["AdminPanel-wrapper-course-title"]}>
+                <label
+                  for="course-title"
+                  className={styles["AdminPanel-wrapper-course-title-label"]}
+                >
+                  Course title
+                </label>
+                <input
+                  name="course-title"
+                  className={styles["AdminPanel-wrapper-course-title-input"]}
+                ></input>
+              </div>
+
+              <div className={styles["AdminPanel-wrapper-course-title"]}>
+                <label
+                  for="course-title"
+                  className={styles["AdminPanel-wrapper-course-title-label"]}
+                >
+                  Section title
+                </label>
+                <input
+                  name="course-title"
+                  className={styles["AdminPanel-wrapper-course-title-input"]}
+                ></input>
+              </div>
+              {/* <div
+                className={styles["AdminPanel-wrapper-course-field-add-button"]}
               >
-                Browse File
-              </Button>
-            </div>
-          </div>
-          <div className={styles["AdminPanel-wrapper-course-title"]}>
-            <label
-              for="course-title"
-              className={styles["AdminPanel-wrapper-course-title-label"]}
-            >
-              Course title
-            </label>
-            <input
-              name="course-title"
-              className={styles["AdminPanel-wrapper-course-title-input"]}
-            ></input>
-          </div>
-          <div className={styles["AdminPanel-wrapper-course-title"]}>
-            <label
-              for="course-title"
-              className={styles["AdminPanel-wrapper-course-title-label"]}
-            >
-              Section title
-            </label>
-            <input
-              name="course-title"
-              className={styles["AdminPanel-wrapper-course-title-input"]}
-            ></input>
-          </div>
-          <div className={styles["AdminPanel-wrapper-course-upload-div"]}>
-            <div className={styles["AdminPanel-wrapper-course-upload"]}>
-              Upload Video <img src={upload} height="24px"></img>
-            </div>
+                <img src={plus} height="24px" width="24px" />
+              </div> */}
 
-            <div className={styles["AdminPanel-wrapper-course-upload"]}>
-              Upload Video <img src={upload} height="24px"></img>
-            </div>
+              {/* <div className={styles["AdminPanel-wrapper-course-upload-div"]}> */}
+              <div className={styles["AdminPanel-wrapper-course-title"]}>
+                <label
+                  for="course-title"
+                  className={styles["AdminPanel-wrapper-course-title-label"]}
+                ></label>
+                <div className={styles["AdminPanel-wrapper-course-upload"]}>
+                  Upload Video <img src={upload} height="24px"></img>
+                </div>
+              </div>
+              <div className={styles["AdminPanel-wrapper-course-title"]}>
+                <label
+                  for="course-title"
+                  className={styles["AdminPanel-wrapper-course-title-label"]}
+                ></label>
+                <div className={styles["AdminPanel-wrapper-course-upload"]}>
+                  Upload Video <img src={upload} height="24px"></img>
+                </div>
+              </div>
+              <div className={styles["AdminPanel-wrapper-course-title"]}>
+                <label
+                  for="course-title"
+                  className={styles["AdminPanel-wrapper-course-title-label"]}
+                ></label>
+                <div className={styles["AdminPanel-wrapper-course-upload"]}>
+                  Upload Video <img src={upload} height="24px"></img>
+                </div>
+                {/* <div
+                className={styles["AdminPanel-wrapper-course-field-add-button"]}
+              >
+                <img src={plus} height="24px" width="24px" />
+              </div> */}
+              </div>
 
-            <div className={styles["AdminPanel-wrapper-course-upload"]}>
-              Upload Video <img src={upload} height="24px"></img>
+              {/* <div className={styles["AdminPanel-wrapper-course-upload"]}>
+            Upload Video <img src={upload} height="24px"></img>
+          </div>
+
+          <div className={styles["AdminPanel-wrapper-course-upload"]}>
+            Upload Video <img src={upload} height="24px"></img>
+          </div> */}
+              {/* </div> */}
+              <div className={styles["AdminPanel-wrapper-course-title"]}>
+                <label
+                  for="course-title"
+                  className={styles["AdminPanel-wrapper-course-title-label"]}
+                >
+                  Uploaded to
+                </label>
+                <input
+                  name="course-title"
+                  className={styles["AdminPanel-wrapper-course-title-input"]}
+                ></input>
+              </div>
+              <div className={styles["AdminPanel-wrapper-course-title"]}>
+                <label
+                  for="course-title"
+                  className={styles["AdminPanel-wrapper-course-title-label"]}
+                >
+                  Course Category
+                </label>
+                <input
+                  name="course-title"
+                  className={styles["AdminPanel-wrapper-course-title-input"]}
+                ></input>
+              </div>
+            </div>
+            <div className={styles["AdminPanel-wrapper-plus"]}>
+              <div
+                className={styles["AdminPanel-wrapper-course-field-add-button"]}
+              >
+                <img src={plus} height="24px" width="24px" />
+              </div>
+              <div
+                className={styles["AdminPanel-wrapper-course-field-add-button"]}
+              >
+                <img src={plus} height="24px" width="24px" />
+              </div>
             </div>
           </div>
-          <div className={styles["AdminPanel-wrapper-course-title"]}>
-            <label
-              for="course-title"
-              className={styles["AdminPanel-wrapper-course-title-label"]}
-            >
-              Uploaded to
-            </label>
-            <input
-              name="course-title"
-              className={styles["AdminPanel-wrapper-course-title-input"]}
-            ></input>
-          </div>
-          <div className={styles["AdminPanel-wrapper-course-title"]}>
-            <label
-              for="course-title"
-              className={styles["AdminPanel-wrapper-course-title-label"]}
-            >
-              Course Category
-            </label>
-            <input
-              name="course-title"
-              className={styles["AdminPanel-wrapper-course-title-input"]}
-            ></input>
+
+          <div className={styles["AdminPanel-wrapper-description"]}>
+            <div className={styles["AdminPanel-wrapper-course-description"]}>
+              <label
+                for="course-description"
+                className={
+                  styles["AdminPanel-wrapper-course-title-label-description"]
+                }
+              >
+                Course Description
+              </label>
+              <textarea
+                rows="20"
+                cols="30"
+                name="course-title"
+                className={
+                  styles["AdminPanel-wrapper-course-description-textarea"]
+                }
+              ></textarea>
+              <textarea
+                rows="20"
+                cols="25"
+                name="course-title"
+                className={
+                  styles[
+                    "AdminPanel-wrapper-course-description-textarea-mobile"
+                  ]
+                }
+              ></textarea>
+            </div>
+            <div className={styles["AdminPanel-wrapper-course-title"]}>
+              <label
+                for="course-title"
+                className={styles["AdminPanel-wrapper-course-title-label-date"]}
+              >
+                Date
+              </label>
+              <input
+                type="date"
+                className={styles["AdminPanel-wrapper-course-title-input-date"]}
+              />
+            </div>
           </div>
         </div>
-        <div className={styles["AdminPanel-wrapper-description"]}></div>
+        <button className={styles["ClientInfo-wrapper-topbar-buttons-save"]}>
+          Save Changes
+        </button>
       </div>
     </div>
   );
