388d3582daa8c34653009ab2ff8a8d46c2c4ece3
abhi-rzoro <144512510+abhi-rzoro@users.noreply.github.com>
2024-05-10 06:44:27 +0530

    add functionality to tools topbar mobile

--- src/CommonComponents/ToolsTopbar/index.jsx
+++ src/CommonComponents/ToolsTopbar/index.jsx
@@ -51,10 +51,49 @@ export default function ToolsTopbar(props) {
         </div>
       </div>
       <div className={styles["Tools-wrapper-topbar-div-mobile"]}>
-        <div className={styles["Tools-wrapper-topbar-text"]}>Sales</div>
-        <div className={styles["Tools-wrapper-topbar-text"]}>Marketing</div>
-        <div className={styles["Tools-wrapper-topbar-text"]}>Product</div>
-        <div className={styles["Tools-wrapper-topbar-text"]}>Calculator</div>
+        <div
+          className={styles["Tools-wrapper-topbar-text"]}
+          onClick={() => props.setDocumentType("sales")}
+          style={{
+            color: props.documentType === "sales" ? "#123c97" : "#a1a1a1",
+            borderColor: props.documentType === "sales" ? "#123c97" : "#a1a1a1",
+          }}
+        >
+          Sales
+        </div>
+        <div
+          className={styles["Tools-wrapper-topbar-text"]}
+          onClick={() => props.setDocumentType("marketing")}
+          style={{
+            color: props.documentType === "marketing" ? "#123c97" : "#a1a1a1",
+            borderColor:
+              props.documentType === "marketing" ? "#123c97" : "#a1a1a1",
+          }}
+        >
+          Marketing
+        </div>
+        <div
+          className={styles["Tools-wrapper-topbar-text"]}
+          style={{
+            color:
+              props.documentType === "productLinks" ? "#123c97" : "#a1a1a1",
+            borderColor:
+              props.documentType === "productLinks" ? "#123c97" : "#a1a1a1",
+          }}
+        >
+          Product
+        </div>
+        <div
+          className={styles["Tools-wrapper-topbar-text"]}
+          onClick={() => props.setDocumentType("calculator")}
+          style={{
+            color: props.documentType === "calculator" ? "#123c97" : "#a1a1a1",
+            borderColor:
+              props.documentType === "calculator" ? "#123c97" : "#a1a1a1",
+          }}
+        >
+          Calculator
+        </div>
       </div>
     </>
   );
