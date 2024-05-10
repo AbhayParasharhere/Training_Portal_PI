333d8fe2ba7749c51f7cd8e008758da52eebc1e4
AbhayParasharhere <83302622+AbhayParasharhere@users.noreply.github.com>
2024-05-10 10:23:15 -0700

    Add modal styling to profile change modal

--- src/Pages/Dashboard/components/ProfileChangeModal/index.jsx
+++ src/Pages/Dashboard/components/ProfileChangeModal/index.jsx
@@ -8,6 +8,7 @@ import {
 import { update } from "firebase/database";
 import { updateNameAndPhoto } from "../../../../Firebase/updateProfile";
 import { AuthContext } from "../../../../context/authContext";
+import styles from "./styles.module.scss";
 
 const ProfileChangeModal = ({ modalOpen, setModalOpen }) => {
   const currentUser = useContext(AuthContext);
@@ -23,34 +24,51 @@ const ProfileChangeModal = ({ modalOpen, setModalOpen }) => {
   console.log("Modal Open: ", modalOpen, photo, name);
   return (
     <Modal
-      title="Basic Modal"
+      title={
+        <span
+          style={{
+            fontSize: "24px",
+            color: "#000",
+            fontFamily: "Epilogue",
+            fontWeight: "600",
+          }}
+        >
+          Update Profile
+        </span>
+      }
       open={modalOpen}
       onOk={handleOk}
       onCancel={() => setModalOpen(false)}
     >
-      <div>
-        <Upload
-          onChange={(info) => {
-            console.log("Info: ", info.file, info);
-            setPhoto(info.file);
-          }}
-          beforeUpload={() => false}
-          maxCount={1}
-        >
-          <AntButton icon={<UploadOutlined />}>Upload Photo</AntButton>
-        </Upload>
+      <div className={styles["wrapper"]}>
+        <div className={styles["input-wrapper"]}>
+          <div className={styles["label"]}>Name: </div>
+          <Input
+            placeholder="Enter Name"
+            prefix={<UserOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+            suffix={
+              <Tooltip title="Please enter the name that you wish to change">
+                <InfoCircleOutlined style={{ color: "rgba(0,0,0,.45)" }} />
+              </Tooltip>
+            }
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className={styles["input-wrapper"]}>
+          <div className={styles["label"]}>Photo: </div>
+          <Upload
+            onChange={(info) => {
+              console.log("Info: ", info.file, info);
+              setPhoto(info.file);
+            }}
+            beforeUpload={() => false}
+            maxCount={1}
+          >
+            <AntButton icon={<UploadOutlined />}>Upload Photo</AntButton>
+          </Upload>
+        </div>
       </div>
-      <Input
-        placeholder="Enter Name"
-        prefix={<UserOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
-        suffix={
-          <Tooltip title="Please enter the name that you wish to change">
-            <InfoCircleOutlined style={{ color: "rgba(0,0,0,.45)" }} />
-          </Tooltip>
-        }
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
     </Modal>
   );
 };
